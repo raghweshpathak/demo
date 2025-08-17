44be82f21c202fe7e4d1bfe89a71d89e86d39811
raghweshpathak <raghwespathak@gmail.com>
2025-08-17 16:28:01 +0530

    modiefied change

---
+++ a.py
@@ -0,0 +1,4 @@
+a=5
+b=6
+print(a+b)
+y=print("hello")
--- ssss.py
+++ ssss.py
@@ -4,5 +4,7 @@ print("sum", a+b)
 
 num  = [0,1,2,3,4,5,6]
 
-for i in range(len(num)-1, -1, -2):
-    print(num[i])
+for i in range(len(num)):
+
+    if num // 2==0:
+        print(num[i])
